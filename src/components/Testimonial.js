f46bc5863a26072b0ab82eb849803db53a3904a0
ernest-emmanuel-utibe <em123nest@gmail.com>
2024-07-30 08:40:58 +0100

    feat: Fixed a bug in Testimonial.js

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,34 +1,34 @@
-import React from "react"
-import { TitleSm } from "./common/Title"
-import Link from "next/link"
-import { testimonial } from "../../src/assets/data/dummydata"
-import { HiOutlineArrowRight } from "react-icons/hi"
-import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
-import Slider from "react-slick"
-import Image from "next/image"
+import React from "react";
+import { TitleSm } from "./common/Title";
+import Link from "next/link";
+import { testimonial } from "../../src/assets/data/dummydata";
+import { HiOutlineArrowRight } from "react-icons/hi";
+import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Image from "next/image";
 
 function SampleNextArrow(props) {
-  const { onClick } = props
+  const { onClick } = props;
   return (
     <div className='slick-arrow'>
       <button className='next' onClick={onClick}>
         <RiArrowRightSLine size={25} />
       </button>
     </div>
-  )
+  );
 }
 
 function SamplePrevArrow(props) {
-  const { onClick } = props
+  const { onClick } = props;
   return (
     <div className='slick-arrow'>
       <button className='prev' onClick={onClick}>
         <RiArrowLeftSLine size={25} />
       </button>
     </div>
-  )
+  );
 }
 
 const Testimonial = () => {
@@ -51,7 +51,7 @@ const Testimonial = () => {
         },
       },
     ],
-  }
+  };
   return (
     <>
       <section className='testimonial'>
@@ -61,12 +61,12 @@ const Testimonial = () => {
           </div>
           <div className='cards'>
             <Slider {...settings}>
-              {testimonial.map((user) => (
-                <div>
+              {testimonial.map((user, index) => (
+                <div key={index}>
                   <div className='card'>
                     <div className='image'>
                       <div className='img'>
-                        <Image src={user.cover} alt='' width={100} height={100}/>
+                        <Image src={user.cover} alt={user.name} width={100} height={100} />
                       </div>
                       <div className='img-text'>
                         <h3>{user.name}</h3>
@@ -87,7 +87,117 @@ const Testimonial = () => {
         </div>
       </section>
     </>
-  )
+  );
 }
 
-export default Testimonial
+export default Testimonial;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react"
+// import { TitleSm } from "./common/Title"
+// import Link from "next/link"
+// import { testimonial } from "../../src/assets/data/dummydata"
+// import { HiOutlineArrowRight } from "react-icons/hi"
+// import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri"
+// import "slick-carousel/slick/slick.css"
+// import "slick-carousel/slick/slick-theme.css"
+// import Slider from "react-slick"
+// import Image from "next/image"
+
+// function SampleNextArrow(props) {
+//   const { onClick } = props
+//   return (
+//     <div className='slick-arrow'>
+//       <button className='next' onClick={onClick}>
+//         <RiArrowRightSLine size={25} />
+//       </button>
+//     </div>
+//   )
+// }
+
+// function SamplePrevArrow(props) {
+//   const { onClick } = props
+//   return (
+//     <div className='slick-arrow'>
+//       <button className='prev' onClick={onClick}>
+//         <RiArrowLeftSLine size={25} />
+//       </button>
+//     </div>
+//   )
+// }
+
+// const Testimonial = () => {
+//   const settings = {
+//     dots: false,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     nextArrow: <SampleNextArrow />,
+//     prevArrow: <SamplePrevArrow />,
+//     responsive: [
+//       {
+//         breakpoint: 800,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           infinite: true,
+//           dots: true,
+//         },
+//       },
+//     ],
+//   }
+//   return (
+//     <>
+//       <section className='testimonial'>
+//         <div className='container'>
+//           <div className='heading-title'>
+//             <TitleSm title='WHAT CLIENTS SAY ABOUT OUR WORK' />
+//           </div>
+//           <div className='cards'>
+//             <Slider {...settings}>
+//               {testimonial.map((user) => (
+//                 <div>
+//                   <div className='card'>
+//                     <div className='image'>
+//                       <div className='img'>
+//                         <Image src={user.cover} alt='' width={100} height={100}/>
+//                       </div>
+//                       <div className='img-text'>
+//                         <h3>{user.name}</h3>
+//                         <span>{user.post}</span>
+//                       </div>
+//                     </div>
+//                     <div className='details'>
+//                       <p>{user.desc}</p>
+//                       <Link href='/#'>
+//                         VIEW CASE <HiOutlineArrowRight className='link-icon' />
+//                       </Link>
+//                     </div>
+//                   </div>
+//                 </div>
+//               ))}
+//             </Slider>
+//           </div>
+//         </div>
+//       </section>
+//     </>
+//   )
+// }
+
+// export default Testimonial
